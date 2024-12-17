3f2da7c15e0356114b03ba41525ef9c9b50d696c
MMolieleng <mjmolieleng@gmail.com>
2024-12-18 00:45:18 +0200

    Clean up

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from models.user import User
 
 
 app = FastAPI()
@@ -6,3 +7,10 @@ app = FastAPI()
 @app.get("/")
 async def hello():
     return {"hellow": "Hello World"}
+
+@app.post("/users/")
+async def create_user(user: User):
+    try:
+        return {'name': user.name}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
